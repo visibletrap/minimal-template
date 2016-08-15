b63531298bf61475e930a0fc30fe48347bbfe7cb
Tap <visibletrap@gmail.com>
2016-08-15 23:38:24 +0800

    Update clojurescript version

--- src/leiningen/new/minimal/clj-figwheel-project.clj
+++ src/leiningen/new/minimal/clj-figwheel-project.clj
@@ -1,6 +1,6 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "{{clojure-version}}"]
-                 [org.clojure/clojurescript "1.9.89"]]
+                 [org.clojure/clojurescript "1.9.211"]]
 
   :plugins [[lein-figwheel "0.5.4-7"]
             [lein-cljsbuild "1.1.3"]]
