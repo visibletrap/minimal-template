4152bd6c9272136a397cf6ab0e450518ae8a30a4
Tap <visibletrap@gmail.com>
2016-08-20 07:58:22 +0800

    Update versions

--- src/leiningen/new/minimal.clj
+++ src/leiningen/new/minimal.clj
@@ -7,7 +7,7 @@
   [name & options]
   (let [data {:name name
               :sanitized (name-to-path name)
-              :clojure-version "1.9.0-alpha10"}
+              :clojure-version "1.9.0-alpha11"}
         figwheel? (and (seq options) (= (first options) "+figwheel"))]
     (cond
       figwheel?
