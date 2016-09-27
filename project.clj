0835004eb3065c45d60c46050d365e936d2a842a
Tap <visibletrap@gmail.com>
2016-09-27 07:40:19 +0700

    Update clojure version to 1.9.0-alpha13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject minimal/lein-template "1.1.12"
+(defproject minimal/lein-template "1.1.13"
   :description "A minimalist template to get started with Clojure project."
   :url "https://github.com/visibletrap/minimal-template"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/minimal.clj
+++ src/leiningen/new/minimal.clj
@@ -7,7 +7,7 @@
   [name & options]
   (let [data {:name name
               :sanitized (name-to-path name)
-              :clojure-version "1.9.0-alpha12"}
+              :clojure-version "1.9.0-alpha13"}
         figwheel? (and (seq options) (= (first options) "+figwheel"))]
     (cond
       figwheel?
