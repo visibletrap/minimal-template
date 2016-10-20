2c2f16178b3cc8a27b0747a7dae95bbcc27c7bad
Tap <visibletrap@gmail.com>
2016-10-20 09:41:36 +0800

    cljs 1.9.293

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject minimal/lein-template "1.1.13"
+(defproject minimal/lein-template "1.1.14"
   :description "A minimalist template to get started with Clojure project."
   :url "https://github.com/visibletrap/minimal-template"
   :license {:name "Eclipse Public License"
