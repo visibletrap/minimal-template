dba089e70601ea1d6deb01c94d61fe4e993793e0
Tap <visibletrap@gmail.com>
2017-08-22 17:34:38 +0700

    Update core.specs.alpha. Add test.check

--- src/leiningen/new/minimal/project.clj
+++ src/leiningen/new/minimal/project.clj
@@ -1,4 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "{{clojure-version}}"]
                  [org.clojure/spec.alpha "0.1.123"]
-                 [org.clojure/core.specs.alpha "0.1.10"]])
+                 [org.clojure/core.specs.alpha "0.1.24"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
