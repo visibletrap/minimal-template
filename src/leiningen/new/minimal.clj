729ee5acbad7fc7d36d7740988d2f57abfb5670f
Nuttanart Pornprasitsakul <nuttanart.p@gmail.com>
2017-09-08 08:06:40 +0700

    Update to alpha 20

--- src/leiningen/new/minimal.clj
+++ src/leiningen/new/minimal.clj
@@ -7,7 +7,7 @@
   [name & options]
   (let [data {:name name
               :sanitized (name-to-path name)
-              :clojure-version "1.9.0-alpha19"} ]
+              :clojure-version "1.9.0-alpha20"} ]
     (->files data
              ["project.clj" (render "project.clj" data)]
              [".gitignore" (render ".gitignore")]
